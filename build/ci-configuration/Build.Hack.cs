55828df852c7e73e788e4c064a6eafe5453fde5a
Matthias Koch <ithrowexceptions@gmail.com>
2020-04-24 02:40:58 +0200

    asd

--- build/ci-configuration/Build.Hack.cs
+++ build/ci-configuration/Build.Hack.cs
@@ -5,20 +5,43 @@
 using System.Reflection;
 using Nuke.Common;
 using Nuke.Common.Tooling;
+using Nuke.Common.Utilities;
+using static Nuke.Common.CI.HandleConfigurationGenerationAttribute;
+using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.Tooling.ProcessTasks;
 
 partial class Build
 {
     public static int Main()
     {
-        if (RootDirectory.ToString().EndsWith("ci-configuration") && Help)
+        if (RootDirectory.ToString().EndsWith("ci-configuration"))
         {
-            StartProcess(
-                    toolPath: Assembly.GetEntryAssembly().NotNull().Location,
-                    arguments: "--root ./ --help",
-                    workingDirectory: RootDirectory.Parent.Parent)
-                .AssertZeroExitCode();
-            return 0;
+            if (Help)
+            {
+                StartProcess(
+                        toolPath: Assembly.GetEntryAssembly().NotNull().Location,
+                        arguments: "--root ./ --help",
+                        workingDirectory: RootDirectory.Parent.Parent)
+                    .AssertZeroExitCode();
+                return 0;
+            }
+
+            if (GetParameter<string>(ConfigurationParameterName) != null &&
+                GetParameter<HostType?>(() => Host) != null)
+            {
+                StartProcess(
+                        toolPath: Assembly.GetEntryAssembly().NotNull().Location,
+                        arguments: $"{CommandLineArguments.JoinSpace()} --root ./",
+                        workingDirectory: RootDirectory.Parent.Parent)
+                    .AssertZeroExitCode();
+                return 0;
+            }
+        }
+
+        if (IsLocalBuild)
+        {
+            Logger.Error("This build cannot be executed locally.");
+            return 1;
         }
 
         return Execute<Build>();
